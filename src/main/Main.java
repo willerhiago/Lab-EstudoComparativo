2b425f60325bc44778e381f27ab7de2eea55fc88
willerhiago <willer.hiago@bol.com.br>
2018-05-13 14:02:36 -0300

    Funfando

--- src/main/Main.java
+++ src/main/Main.java
@@ -9,10 +9,14 @@ public class Main {
     public static void main(String[] args) {
     	
     	Comparacao comp = new Comparacao();
-    	int tamArray = 1000;
+    	int tamArray = 10000;
     	int n  = 100; //quantidade de iteracoes
     	//comp.bubble(tamArray, n);
         //comp.heap(tamArray, n);
-    	comp.insertion(tamArray, n);
+    	//comp.insertion(tamArray, n);
+    	//comp.merge(tamArray, n);
+    	//comp.quick(tamArray, n);
+    	comp.selection(tamArray, n);
+    	//comp.shell(tamArray, n);
     }
 }
