8e8a0fa21177da3948939bf0720d0258c28e2534
willerhiago <willer.hiago@bol.com.br>
2018-05-13 00:33:37 -0300

    Desvio e media

---
+++ src/sorts/ComparacaoTeste.java
@@ -0,0 +1,52 @@
+package sorts;
+
+public class ComparacaoTeste {
+	
+	private int[] valores;
+	private int media;
+	private double desvioPadrao;
+	private int qntElem;
+	
+	public void calculaMediaEDesvio(int[] valores) {
+		this.valores = valores;
+		this.qntElem = valores.length;
+		int soma = 0;
+		for(int i = 0; i < qntElem;i++){
+			soma += valores[i];
+		}
+		this.media = (soma/qntElem);
+		this.calculaDesvio();
+	}
+	
+	public void calculaDesvio() {
+		double somaVariancia = 0;
+		int variacao;
+		
+		for(int i = 0; i < qntElem;i++){
+			variacao = valores[i] - this.media;
+			somaVariancia += (int)Math.pow(variacao,2);
+			
+		}
+		this.desvioPadrao =  Math.sqrt(somaVariancia/(qntElem - 1)); 
+	}
+
+	
+	//--------------------------------Getters
+	public int[] getValores() {
+		return valores;
+	}
+
+	public int getMedia() {
+		return media;
+	}
+
+	public double getDesvioPadrao() {
+		return desvioPadrao;
+	}
+
+	public int getQntElem() {
+		return qntElem;
+	}
+	
+	
+}
